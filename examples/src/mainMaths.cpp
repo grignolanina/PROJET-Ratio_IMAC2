cd19057be770317704fbfa1874b36838b8e0d4a7
Pauline Gobé <pauline.gobe16@gmail.com>
2022-12-15 15:08:36 +0100

    sqrt + cos + testMaths

---
+++ examples/src/mainMaths.cpp
@@ -0,0 +1,63 @@
+// *** This main is used to do mathematical test to see how use some opererators with ratios. 
+// All the explainations needed are in the report file of the project *** 
+
+#include <iostream>
+#include <cmath>
+#include "Ratio.hpp"
+#include <iomanip>
+
+int main(){
+
+    // ----- 1. SQUARE ROOT -----
+
+    // We want to know what is better between doing a square root on a ratio or doing
+    //it with the numerator and denominator separately 
+
+
+    //enter values for num and denom
+    int num = 1;
+    int denom = 4;
+    //enter the exact value if knowned
+    double exactValue = 0.5;
+
+    //ratio of the previous numbers
+    Ratio<int> r1(num,denom);
+    Ratio<int> r2(r1);
+
+    //Ratio<int> r3 = r1 * 2 + r2 * 2;
+    
+    //sqrt of the ratio 
+    double racineRatio = r1.sqrt();
+
+    //sqrt of the separte numbers
+    //double racineDouble = sqrt(r3.getNum())/sqrt(r3.getDenom());
+    double racineDouble = sqrt((double)num/denom);
+
+    std::cout << "sqrt(ratio) : " << std::setprecision(30) << racineRatio << std::endl;
+    std::cout << "sqrt(num)/sqrt(denum)  : " << std::setprecision(30) << racineDouble << std::endl;
+
+    std::cout << "Difference between both sqrt : " << std::setprecision(20) << std::abs(racineDouble - racineRatio) << std::endl;
+    std::cout << "Difference with exact value (ratio) : " << std::setprecision(20) << std::abs(racineRatio - exactValue) << std::endl;
+    std::cout << "Difference with exact value : " << std::setprecision(20) << std::abs(racineDouble- exactValue) << std::endl;
+
+    
+    // ----- 2. COSINUS -----
+
+    Ratio<int> piSurTrois = r1.pi()/3;
+    double exactValueCos = 0.5;
+
+    double cosRatio = piSurTrois.cosinus();
+    double cosRatio2 = piSurTrois.cosinus2();
+
+    std::cout << "cos1(ratio) : " << std::setprecision(20) << cosRatio << std::endl;
+    std::cout << "cos2(ratio)  : " << std::setprecision(20) << cosRatio2 << std::endl;
+
+    std::cout << "Difference between both sqrt : " << std::setprecision(20) << std::abs(cosRatio - cosRatio2) << std::endl;
+    std::cout << "Difference with exact value : " << std::setprecision(20) << std::abs(cosRatio - exactValueCos) << std::endl;
+
+
+
+    
+
+    return 0; 
+}
--- myLib/include/Ratio.hpp
+++ myLib/include/Ratio.hpp
@@ -2,6 +2,7 @@
 #include <numeric>
 #include <string>
 #include <iostream>
+#include <cmath>
 
 #include "RatioException.hpp"
 
@@ -187,6 +188,19 @@ class Ratio {
 	/// \return the absolute value of the ratio
 	Ratio abs();
 
+	/// \brief fonction which gives the square root of a ratio
+	/// \return the square root value of the ratio
+	double sqrt();
+
+	/// \brief fonction which gives the cosinus of a ratio
+	/// \return the value of the cosinus of the ratio
+	double cosinus();
+
+	/// \brief fonction which gives an approciamtion of the cosinus of a ratio
+	/// \return the value of the approximated cosinus of the ratio
+	double cosinus2();
+
+
 	//PARTIE ENTIERE
 	//COS 
 	//SIN
@@ -230,6 +244,10 @@ class Ratio {
 	//verif les constexpr static noexcept et les mettre en bas
 
 	//pas reellement des variables statiques, voir comment faire
+
+	/// \brief fonction which givr the pi ratio
+	/// \return ratio 22/7
+	constexpr static Ratio pi()noexcept{return Ratio(M_PI/1);};
 
 
 
@@ -357,6 +375,21 @@ Ratio<T> Ratio<T>::abs(){
 	}else{
 		return (*this);
 	}
+}
+
+template<typename T>
+double Ratio<T>::sqrt(){  
+	return std::sqrt(m_num)/std::sqrt(m_denom);
+}
+
+template<typename T>
+double Ratio<T>::cosinus(){  
+	return std::cos((double)m_num/m_denom);
+}
+
+template<typename T>
+double Ratio<T>::cosinus2(){  
+	return std::cos(m_num)/std::cos(m_denom);
 }
 
 /*****************************************************************
