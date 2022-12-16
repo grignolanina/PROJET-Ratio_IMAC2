ca0f624a99fd4d39657404e9f5f03aebbe0d1a87
grignolanina <grignola.nina@orange.fr>
2022-12-16 11:34:48 +0100

    lib ok

--- myLib/include/Ratio.hpp
+++ myLib/include/Ratio.hpp
@@ -10,6 +10,28 @@
 //static const unsigned int NB_ITER = 5;
 //dans l'idée ça pour PI mais en dehors de la classe
 
+// Doxygen menu
+/// \version 0.1
+/// \mainpage
+/// \image html accueil.jpg
+/// \tableofcontents
+/// \section instroduction_sec What for?
+/// To handle rationnals
+/// \section install_bigsec How to install
+/// \subsection dependencies_sec Dependecies
+/// \li nothing
+/// \li Doxygen (if you want the documentation)
+/// \subsection install_sec Install with cmake (Linux / Mac)
+/// \li go to main dir
+/// \li mkdir build
+/// \li cd build
+/// \li cmake ..
+/// \li make
+/// \li if Doxygen installed: make html
+/// \li The documentation is located in :
+/// 	- [path to build]/doc/doc-doxygen/html/index.html or 
+/// 	- or [path to build]/INTERFACE/doc/doc-doxygen/html/index.html
+
 /// \class Ratio
 /// \brief class defining a rational for linear algebra operations
 template<typename T>
