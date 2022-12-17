cce69aeb591ab904f5ef7217b40c00f57bdc52d1
grignolanina <grignola.nina@orange.fr>
2022-12-17 22:49:03 +0100

    doxygen ok

--- myLib/include/Ratio.hpp
+++ myLib/include/Ratio.hpp
@@ -17,35 +17,26 @@
 /// \tableofcontents
 /// \section instroduction_sec Ratio projet
 /// This is a student project realised at the French engineering school IMAC, for the third semester for object programming and computer science mathematics courses.This work focused on the representation of floating point numbers in the form of rationals. You will therefore find there a research work on rational numbers, the mathematical rules that apply to them and a library allowing their manipulation.
-/// \section install_bigsec Dependecies
-/// \li Dependencies
-/// \li cmake (at least version  3.13)
-/// \li
+/// \section dependencies_sec Dependencies
+/// \subsection is CMake (at least version  3.13)
 /// \li - Linux : sudo apt install cmake
 ///\li - Mac : brew install cmake
 ///\li - Windows : https://cmake.org/install
-///\li
-///\li Doxygen
-///\li
+/// \subsection Doxygen
 ///\li - Linux : sudo apt install doxygen
 ///\li - Mac : brew install doxygen
 ///\li - Windows : https://www.doxygen.nl/manual/install.html
-///\li
-///\li Google Test
-///\li
+/// \subsection Google Test
 ///\li - Linux : sudo apt-get install libgtest-dev
 ///\li - Mac : 
-///\li
-/// \li       cd /tmp
-/// \li       git clone https://github.com/google/googletest.git
-/// \li         cd googletest
-/// \li        mkdir build && cd build
-/// \li         cmake ..
-/// \li         make && sudo make install
-/// \li- Windows : ?
-/// \li
-/// \section install_bigsec Usage
-/// \li 
+/// \n       cd /tmp
+/// \n       git clone https://github.com/google/googletest.git
+/// \n       cd googletest
+/// \n       mkdir build && cd build
+/// \n       cmake ..
+/// \n       make && sudo make install
+/// \li - Windows : ?
+/// \section usage_sec Usage
 /// \li Please use the CMake in the IDE : VSCode
 /// \li Before starting the project make sure you have the following extensions :
 /// \li • C/C++ (microsoft)
@@ -53,8 +44,7 @@
 /// \li • C/C++ Themes (microsoft)
 /// \li • Clang-Format (xaver)
 /// \li • CMake (twxs)
-/// \section install_bigsec Authors
-///\li
+/// \section author_sec Authors
 /// \li GRIGNOLA Nina
 /// \li GOBE Pauline
 
